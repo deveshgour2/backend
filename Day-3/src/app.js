45a63871f831414abad9ee74d530a667312d48b8
deveshgour2 <deveshgour6@gmail.com>
2026-09-22 23:39:42 +0530

    Day-3

---
+++ Day-3/src/app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+
+const  app = express()
+
+app.use(express.json())
+
+const notes = []
+
+app.post('/notes',(req, res)=>{
+    notes.push(req.body)
+    res.send("Server created successfully")
+})
+
+app.get('/notes',(req,res)=>{
+    res.send(notes)
+})
+
+app.delete('/notes/:index',(req, res)=>{
+    delete notes[req.params.index]
+    res.send('notes deleted successfully')
+})
+
+app.patch('/notes/:index',(req, res)=>{
+    notes[req.params.index].description = req.body.description
+    res.send('notes modified successfully')
+})
+
+module.exports = app
