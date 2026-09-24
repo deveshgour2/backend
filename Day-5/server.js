9d969db9666a132470219b6a9668a6df928dcd7f
deveshgour2 <deveshgour6@gmail.com>
2026-09-24 23:33:40 +0530

    created first database

--- Day-5/server.js
+++ Day-5/server.js
@@ -2,15 +2,16 @@ const app = require('./src/app')
 
 const mongoose = require('mongoose')
 
-function connectToDb(){
-    mongoose.connect('mongodb+srv://deveshgour6_db_user:skxQ5Rg4NKk0EJKW@cluster0.x2d3ekq.mongodb.net/Day-5')
-
-    console.log('Database created')
+function connectToDb() {
+    mongoose.connect('')
+    .then(()=>{
+         console.log('Database created')
+    })
 }
 
 connectToDb()
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("server running at port 3000");
-    
+
 })
