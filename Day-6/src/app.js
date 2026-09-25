b772c188b8da7b60770ab8372159c1d882a29af8
deveshgour2 <deveshgour6@gmail.com>
2026-09-26 02:07:34 +0530

    crud  operations

--- Day-6/src/app.js
+++ Day-6/src/app.js
@@ -28,4 +28,29 @@ app.get('/notes',async (req, res)=>{
 })
 
 
+app.delete('/notes/:id', async (req, res)=>{
+    const notes = await noteModels.deleteOne({
+       _id :  req.params.id
+    })
+
+    res.status(204).json({
+        message:'note deleted successfully',
+        
+    })
+})
+
+app.patch('/notes/:id',async (req, res)=>{
+    const {description}  = req.body
+    const notes = await noteModels.findByIdAndUpdate(
+        req.params.id,
+        { description},
+        {new:true}
+    )
+
+    res.status(200).json({
+        message: 'note updated successfully',
+        notes
+    })
+})
+
 module.exports = app 
